0ba07c3382e3877b444f45bca8ce217b3b1e88b4
cloudymax <admin@cloudydev.net>
2022-11-20 12:47:30 +0000

    adding dockerfile and starting to debug this again

---
+++ Dockerfile
@@ -0,0 +1,92 @@
+FROM debian:bookworm
+
+ENV GEKKO_VERSION="0.32.0"
+ENV HUB_VERSION="3.3.0"
+ENV EDITOR_VERSION="2022.1.23f1"
+ENV CHANGE_SET="9636b062134a"
+ENV USN="email"
+ENV PSWD="password"
+ENV LICENSE_NAME="Unity_v${EDITOR_VERSION}.alf"
+
+RUN mkdir -p "/opt/unityhub/chrome-sandbox" && mkdir -p "/opt/unity/editors"
+
+# Apt deps installation and user creation.
+# Add the out-of-date ssl package as workaround for known issue
+RUN apt-get update && \
+    apt-get upgrade -y && \
+    apt-get install -y \
+    apt-utils \
+    ca-certificates \
+    libasound2 \
+    libc6-dev \
+    libcap2 \
+    libgconf-2-4 \
+    libglu1 \
+    libgtk-3-0 \
+    libncurses5 \
+    libnotify4 \
+    libnss3 \
+    libxtst6 \
+    libxss1 \
+    cpio \
+    lsb-release \
+    python3-pip \
+    docker.io \
+    python-setuptools \
+    xz-utils \
+    #atop \
+    curl \
+    git \
+    git-lfs \
+    jq \
+    openssh-client \
+    wget \
+    software-properties-common \
+    zenity \
+    libgbm1 \
+    gnupg \
+    xvfb \
+    sudo \
+    python3 \
+    python3-pip \
+    firefox-esr && \
+    rm -rf /var/lib/apt/lists/* && \
+    useradd -ms /usr/sbin/nologin runner && \
+    usermod -aG sudo runner && \
+    echo 'runner ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers && \
+    wget http://security.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1f-1ubuntu2_amd64.deb && \
+    sudo dpkg -i libssl1.1_1.1.1f-1ubuntu2_amd64.deb && \
+    rm libssl1.1_1.1.1f-1ubuntu2_amd64.deb
+
+# Unity-Hub install
+RUN sh -c 'echo "deb https://hub.unity3d.com/linux/repos/deb stable main" > /etc/apt/sources.list.d/unityhub.list' \
+ && wget -qO - https://hub.unity3d.com/linux/keys/public | apt-key add - \
+ && apt-get -q update \
+ && apt-get -q install -y "unityhub=$HUB_VERSION" \
+ && apt-get clean
+
+# Alias unityhub to unity-hub which adds a virtual framebuffer and runs in
+# headless mode. then run set the default install path.
+RUN echo '#!/bin/bash\nxvfb-run -ae /dev/stdout /opt/unityhub/unityhub-bin --no-sandbox --headless "$@"' > /usr/bin/unity-hub && \
+ chmod +x /usr/bin/unity-hub && \
+ export APP_DIR=$(echo "/opt/unity/editors/$EDITOR_VERSION/Editor") && \
+ xvfb-run -ae /dev/stdout /opt/unityhub/unityhub-bin --no-sandbox --headless install-path --set "/opt/unity/editors"
+
+# Install the unity editor version (changeset required) to the new path
+RUN unity-hub install --version $EDITOR_VERSION --changeset $CHANGE_SET | tee /var/log/install-editor.log && grep 'Error' /var/log/install-editor.log | exit $(wc -l)
+
+# Switch to the user account and user home directory
+USER runner
+WORKDIR /home/runner
+
+# Install selenium for automating firefox
+RUN pip3 install selenium && \
+    sudo mkdir -p ~/.local/bin && \
+    export GEKKO_URL=$(echo "https://github.com/mozilla/geckodriver/releases/download/v${GEKKO_VERSION}/geckodriver-v${GEKKO_VERSION}-linux64.tar.gz") && \
+  sudo wget $GEKKO_URL && \
+  sudo tar xvfz geckodriver-v"${GEKKO_VERSION}"-linux64.tar.gz && \
+  sudo rm geckodriver-v"${GEKKO_VERSION}"-linux64.tar.gz && \
+  sudo mv geckodriver ~/.local/bin
+
+# Get an alf file
+RUN /opt/unity/editors/2022.1.23f1/Editor/Unity -quit -batchmode -/opt/unity/editors/2022.1.23f1/Editor/Unity -quit -batchmode -nographics -logFile /dev/stdout -createManualActivationFile -username "username" -password "password"
--- program/license.py
+++ program/license.py
@@ -1,54 +1,61 @@
 #!/usr/bin/python3
-import sys                                          # cli args
-import os                                           # file paths
-import time                                         # sleep
-import json                                         # json data
-import io_tools as io # custom lib
+import sys
+import os
+import time
+import io_tools as io
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
-user_name    = "username"                      # needed to log in to unity account
-password     = "password"                      # needed to log in to unity account
+
+user_name = "username"
+password = "password"
 
 tmp_file_dir = "html_reference"
 
-license_file = sys.argv[1]                                  # license file comes from the command line and an arg
-license_path = os.getcwd() + "/license/" + license_file     # path to license file
+license_file = sys.argv[1]
+license_path = os.getcwd() + "/license/" + license_file
 
 config_file = sys.argv[2]
 config_path = os.getcwd() + "/config/" + config_file
+
 
 class radio_button():
     Name = ""
     xpath = ""
     text = ""
 
+
 class config():
     elements = {}
     urls = {}
 
-def element_by_xpath(driver, xpath, debug=False):   #returns html element
+
+def element_by_xpath(driver, xpath, debug=False):
     try:
         wait = WebDriverWait(driver, timeout=6)
         element = driver.find_element_by_xpath(xpath)
         io.print_pretty(f"lookup successfull: found {element} using the path: {xpath}", debug)
-    except:
+        return element
+    except Exception as err:
         io.print_pretty(f"cant find the element w/ xpath {xpath}", True)
         print_page(driver, "element_by_xpath_error")
-    return element
+        return err
+
 
 def element_by_id(driver, elementId, debug=False):  # returns html element
     try:
         element = WebDriverWait(driver, 10).until(ec.presence_of_element_located((By.ID, elementId)))
         io.print_pretty(f"lookup successfull: found {element} using the id: {elementId}", debug)
-    except:
+        return element
+    except Exception as err:
         io.print_pretty(f"cant find the elemtn w/ path {elementId}", True)
         print_page(driver, "element_by_id_error", True)
-    return element
+        return err
 
-def click_on_ready(driver, element, debug=False):   # clicks on element when it is ready
+
+def click_on_ready(driver, element, debug=False):
 
     wait = WebDriverWait(driver, 10)
 
@@ -56,13 +63,13 @@ def click_on_ready(driver, element, debug=False):   # clicks on element when it 
         wait.until(ec.element_to_be_clickable(element.__dict__))
         element.click()
         io.print_pretty(f"click successful on element {element}")
-    except:
+    except Exception as err:
         io.print_pretty(f"click failed on element {element}", True)
         print_page(driver, "click_element_error", True)
 
-def login(driver, settings, debug=False):           # attempts to log in to web page
+def login(driver, settings, debug=False):
 
-    #get to unity id login page
+    # get to unity id login page
     driver.get(settings['urls']['login'])
 
     # populate the username field
@@ -82,9 +89,10 @@ def login(driver, settings, debug=False):           # attempts to log in to web 
     login_button = element_by_xpath(driver, button_name)
     click_on_ready(driver, login_button, debug)
 
-    #driver.find_element_by_xpath(settings['config']['login_button']).click()
+    # driver.find_element_by_xpath(settings['config']['login_button']).click()
 
-def unity_auth_upload(driver):                      # attempts to upload a .alf file to the unity license server
+
+def unity_auth_upload(driver):
 
     # get the liscence activation page
     driver.get(unity_license_url)
@@ -99,38 +107,53 @@ def unity_auth_upload(driver):                      # attempts to upload a .alf 
     webElement = driver.find_element_by_xpath(button_class_name)
     click_only(driver, webElement)
 
-def select_license_type(driver):                    # attempts to select one of the radio button objects
+
+def select_license_type(driver):
+    # attempts to select one of the radio button objects
     print_page(driver, "activation_type_selection")
     # get the available options
 
     # click the Unity Personal option
-    #driver.find_element_by_xpath(login_button).click()
+    # driver.find_element_by_xpath(login_button).click()
 
-def click_only(driver, webElement):                 # generic function to perform a mouse click ONLY on a webElement
+
+def click_only(driver, webElement):
+    # generic function to perform a mouse click ONLY on a webElement
+
     action = ActionChains(driver)
     action.move_to_element(webElement).click(on_element=webElement)
     action.perform()
     time.sleep(2)
 
-def click_and_release(driver, webElement):          # generic function to perform a mouse click  AND release on a webElement
+
+def click_and_release(driver, webElement):
+    # generic function to perform a mouse click  AND release on a webElement
+
     action = ActionChains(driver)
     action.move_to_element(webElement).click(on_element=webElement).release(on_element=webElement)
     action.perform()
     time.sleep(2)
 
-def print_page(driver, name, debug):                # print the page source of the dirver to a .html file
-    """ helpful when you need to see the HTML and find the element names because the
-         license page will forcefully redirect you if you are not logged in                      """
+
+def print_page(driver, name, debug=True):
+    # print the page source of the dirver to a .html file
+
+    """
+    helpful when you need to see the HTML and find the element names because
+    the license page will forcefully redirect you if you are not logged in
+    """
+
     path = f"html_reference/{name}.html"
     io.write_file(path, driver.page_source, debug)
+
 
 def main():                                         # main program
     debug = True
     go_steppy = False
 
-    #run FireFox in headless mode
+    # run FireFox in headless mode
     opts = webdriver.FirefoxOptions()
-    opts.headless = False
+    opts.headless = True
     assert not opts.headless
 
     driver = webdriver.Firefox(options=opts)
@@ -145,9 +168,9 @@ def main():                                         # main program
     username = settings['user']['username']
     password = settings['user']['password']
 
-
     login(driver, settings, debug)
     unity_auth_upload(driver)
     select_license_type(driver)
+
 
 main()
