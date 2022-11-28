08ed7a450da5e1d609a2b6a20b2f42ce3e329c6f
cloudymax <admin@cloudydev.net>
2022-11-28 18:52:53 +0000

    lint and cleanup, made headless again

--- license.py
+++ license.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 import sys
 import os
 import time
@@ -8,6 +9,9 @@ from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
+
+import time
+
 
 tmp_file_dir = "html_reference"
 
@@ -36,7 +40,7 @@ def element_by_xpath(driver, xpath, debug=False):
     try:
         wait = WebDriverWait(driver, timeout=10)
         element = driver.find_elements(By.XPATH, xpath)
-        io.print_pretty(f"lookup successfull: found {element} using the path: {xpath}", debug)
+        io.print_pretty(f"found {element} using the path: {xpath}", debug)
         return element[0]
     except Exception as err:
         io.print_pretty(f"cant find the element w/ xpath {xpath}", True)
@@ -50,8 +54,9 @@ def element_by_id(driver, elementId, debug=False):  # returns html element
     This functions will search for a webElement by it's ID
     """
     try:
-        element = WebDriverWait(driver, 10).until(ec.presence_of_element_located((By.ID, elementId)))
-        io.print_pretty(f"lookup successfull: found {element} using the id: {elementId}", debug)
+        element = WebDriverWait(driver, 10).until(
+                ec.presence_of_element_located((By.ID, elementId)))
+        io.print_pretty(f"found {element} using the id: {elementId}", debug)
         return element
     except Exception as err:
         io.print_pretty(f"cant find the elemtn w/ path {elementId}", True)
@@ -62,8 +67,8 @@ def element_by_id(driver, elementId, debug=False):  # returns html element
 
 def click_on_ready(driver, element, debug=True):
     """
-    This function will wait until the specified webElement is in a Ready state and
-    then perform a click action.
+    This function will wait until the specified webElement is in a
+    Ready state and then perform a click action.
     """
     try:
         wait = WebDriverWait(driver, 10)
@@ -76,11 +81,12 @@ def click_on_ready(driver, element, debug=True):
         print(err)
         return err
 
+
 def login(driver, settings, debug=False):
     """
-    This function will open the unity login page and attempt to authenticate using
-    the provided credentials. This provides the session we will use throughout the 
-    authentication process.
+    This function will open the unity login page and attempt to
+    authenticate using the provided credentials. This provides the session
+    we will use throughout the authentication process.
     """
 
     # get to unity id login page
@@ -103,57 +109,61 @@ def login(driver, settings, debug=False):
     button_name = settings['config']['login_button']
     login_button = element_by_xpath(driver, button_name)
     click_on_ready(driver, login_button, debug)
-    
+
 
 def unity_auth_upload(driver, settings, debug=False):
     """
-    This function will select the Unity .alf file and upload it to the 
+    This function will select the Unity .alf file and upload it to the
     authentication service.
     """
-    
+
     # get the liscence activation page
     driver.get(settings['urls']['license'])
 
     # get the file upload element and pass it our license file
     # We have to do a sleep here or Unity will clear our inputs
-    import time
     time.sleep(5)
     driver.find_element(By.ID, settings['config']['file_elementId']).send_keys(license_path)
-    io.print_pretty(f"Located the file upload file field.", debug)
-        
+    io.print_pretty("Located the file upload file field.", debug)
+
     # submit the file to the unity license server
     # will redirect you forcibly to the unity account login if you arent using
     # a logged-in session
-    webElement = driver.find_element(By.XPATH, settings['config']['button_class_name'])
-    io.print_pretty(f"Located the file upload button.", debug)
+    webElement = driver.find_element(
+            By.XPATH, settings['config']['button_class_name'])
+    io.print_pretty("Located the file upload button.", debug)
 
     click_only(driver, webElement)
-    io.print_pretty(f"Successfully clicked the upload button", debug)
-    
+    io.print_pretty("Successfully clicked the upload button", debug)
     select_license_type(driver, settings, debug)
+
 
 def select_license_type(driver, settings, debug=False):
     """
-    This function will select the appropriate options for requesting a 
+    This function will select the appropriate options for requesting a
     personal license
     """
 
     # click the Unity Personal option
-    webElement = driver.find_element(By.XPATH, settings['radio_buttons']['Personal'])
+    webElement = driver.find_element(
+            By.XPATH, settings['radio_buttons']['Personal'])
     click_on_ready(driver, webElement, debug)
-    
+
     # click an option
-    webElement = driver.find_element(By.XPATH, settings['radio_buttons']['nosale']) 
+    webElement = driver.find_element(
+            By.XPATH, settings['radio_buttons']['nosale'])
     click_on_ready(driver, webElement, debug)
-    
+
     # submit choice
-    webElement = driver.find_element(By.XPATH, settings['config']['license_options_submit'])
+    webElement = driver.find_element(
+            By.XPATH, settings['config']['license_options_submit'])
     click_on_ready(driver, webElement, debug)
 
     time.sleep(2)
 
     # download the license file to ~/Downloads
-    webElement = driver.find_element(By.XPATH, settings['config']['download_license']).click()
+    webElement = driver.find_element(
+            By.XPATH, settings['config']['download_license']).click()
 
 
 def click_only(driver, webElement):
@@ -169,7 +179,8 @@ def click_and_release(driver, webElement):
     # generic function to perform a mouse click  AND release on a webElement
 
     action = ActionChains(driver)
-    action.move_to_element(webElement).click(on_element=webElement).release(on_element=webElement)
+    action.move_to_element(webElement).click(
+            on_element=webElement).release(on_element=webElement)
     action.perform()
     time.sleep(2)
 
@@ -188,32 +199,27 @@ def print_page(driver, name, debug=True):
 
 def main():
     """
-    This program will attempt to automatically create, upload, 
+    This program will attempt to automatically create, upload,
     and authorize a unity personal licesnse.
     """
 
     # Set if FireFox runs in headless mode
     opts = webdriver.FirefoxOptions()
-    opts.headless = False
-    assert not opts.headless
+    opts.headless = True
+    assert opts.headless
 
     # Instantiate the gekko driver
     driver = webdriver.Firefox(options=opts)
     driver.implicitly_wait(10)
-    
+
     # Read settings from jsonfile
     debug = True
     go_steppy = False
     json_data = io.read_file(config_file)
-    
+
     # Convert json data into dict
     vars = io.Variables(json_data, debug, go_steppy)
     settings = vars.settings
-   
-    # Load initial values from settings
-    login_page = settings['urls']['login']
-    username = settings['user']['username']
-    password = settings['user']['password']
 
     # Perform authentication steps
     login(driver, settings, debug)
